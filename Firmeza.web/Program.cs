128487371e3c3d4f7aafefaa486226dc26f970a6
iquintero24 <i.quintero@civitran.com>
2025-10-27 23:45:37 -0500

    feat: configuration of the Dbcontext and connectionString

--- Firmeza.web/Program.cs
+++ Firmeza.web/Program.cs
@@ -1,4 +1,38 @@
+
+using DotNetEnv;
+using Firmeza.web.Data;
+using Microsoft.EntityFrameworkCore;
+
+// load the environment variables:
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// get the connection string 
+var connectionString = builder.Configuration.GetValue<string>("SUPABASE_CONNECTION_STRING");
+
+// (*) using the direct connection for migrations:
+
+if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development" && args.Contains("database update"))
+{
+    connectionString = builder.Configuration.GetValue<string>("SUPABASE_MIGRATION_STRING");
+}
+
+// (*) conditional if the connection string is not found:
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("Connection string not found: SUPABASE_CONNECTION_STRING");
+}
+
+// (*) configuration of the dbContext for use Npgsql (Driver) and the connection pooler;
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString, npgsqlOptions =>
+    {
+        npgsqlOptions.EnableRetryOnFailure(
+            maxRetryCount:5,
+            maxRetryDelay:TimeSpan.FromSeconds(30),
+            errorCodesToAdd:null);
+    }));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
