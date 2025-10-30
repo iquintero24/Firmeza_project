6bc76471a1c54153de07ecbff6b65d39e6c317fd
iquintero24 <isaacquintero4k@gmail.com>
2025-10-29 19:50:38 -0500

    wip: implement GenericRepository base structure for Product entity

---
+++ Firmeza.web/Repositories/Interfaces/Base/IGenericRepository.cs
@@ -0,0 +1,10 @@
+namespace Firmeza.web.Repositories.Interfaces.Base;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(int id);
+}
---
+++ Firmeza.web/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,9 @@
+using Firmeza.web.Data.Entities;
+using Firmeza.web.Repositories.Interfaces.Base;
+
+namespace Firmeza.web.Repositories.Interfaces;
+
+public interface IProductRepository: IGenericRepository<Product>
+{
+    // this agg method specific that product need 
+}
