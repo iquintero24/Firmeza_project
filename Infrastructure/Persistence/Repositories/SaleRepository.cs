55c0fc1be2412979d9622c4e116810e3cab166ec
iquintero24 <i.quintero@civitran.com>
2025-11-13 00:54:36 -0500

    feat: crud sale finish

--- Infrastructure/Persistence/Repositories/SaleRepository.cs
+++ Infrastructure/Persistence/Repositories/SaleRepository.cs
@@ -1,12 +1,33 @@
 using Firmeza.Domain.Entities;
 using Firmeza.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Firmeza.Infrastructure.Persistence.Repositories;
 
 public class SaleRepository : GenericRepository<Sale>, ISaleRepository
 {
+    private readonly ApplicationDbContext _context;
+
     public SaleRepository(ApplicationDbContext dbContext) : base(dbContext)
     {
-        
+        _context = dbContext;
+    }
+
+    public override async Task<IEnumerable<Sale>> GetAllAsync()
+    {
+        return await _context.Sales
+            .Include(s => s.Customer)          
+            .Include(s => s.SaleDetails)       
+            .ThenInclude(d => d.Product)     
+            .ToListAsync();
+    }
+
+    public override async Task<Sale?> GetByIdAsync(int id)
+    {
+        return await _context.Sales
+            .Include(s => s.Customer)
+            .Include(s => s.SaleDetails)
+            .ThenInclude(d => d.Product)
+            .FirstOrDefaultAsync(s => s.Id == id);
     }
 }
